5a8bd81774f2fa5d262648b70fca09fbdc8a4386
saadmanrafat <saadman@outlook.com>
2020-12-08 06:43:38 +0600

    TestHideReplies

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -4,11 +4,10 @@ from twitter_stream import RecentSearch
 
 
 class TestRecentSearch(unittest.TestCase):
-
     class Search(RecentSearch):
-        query = ['python']
-        max_results = ['10']
-        tweet_fields = ['created_at', 'lang', 'conversation_id']
+        query = ["python"]
+        max_results = ["10"]
+        tweet_fields = ["created_at", "lang", "conversation_id"]
 
     def setUp(self) -> None:
         self.search = self.Search()
@@ -16,14 +15,13 @@ class TestRecentSearch(unittest.TestCase):
             self._response = tweet
 
     def test_result_count(self):
-        assert self._response['meta']['result_count'] == 10
+        assert self._response["meta"]["result_count"] == 10
 
     def test_response_has_created_at(self):
-        self.assertTrue(self._response['data'][0]['created_at'], 'created_at')
+        self.assertTrue(self._response["data"][0]["created_at"], "created_at")
 
     def test_response_has_lang(self):
-        self.assertTrue(self._response['data'][0]['lang'], 'lang')
+        self.assertTrue(self._response["data"][0]["lang"], "lang")
 
     def test_response_has_conversation_id(self):
-        self.assertTrue(self._response['data'][0]['conversation_id'], 'conversation_id')
-
+        self.assertTrue(self._response["data"][0]["conversation_id"], "conversation_id")
